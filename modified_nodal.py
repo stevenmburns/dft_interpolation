585b66dda17d48491c7a04288b159a052b0b2b7f
Steven Burns <smburns47@gmail.com>
2020-08-23 14:37:19 -0700

    Added more examples

--- modified_nodal.py
+++ modified_nodal.py
@@ -2,16 +2,14 @@
 import numpy as np
 from scipy.sparse import dok_matrix, csc_matrix, linalg as sla
 
+
 class Element:
     def __init__(self):
         pass
 
-    def sens(self, mn):
-        print("Sensitivity not implemented...")
-        return 0.0
 
 class TwoTerminalElement(Element):
-    def __init__(self,i,j,extra_row_cols=0):
+    def __init__(self, i, j, extra_row_cols=0):
         self.i = i
         self.j = j
         self.extra_row_cols = extra_row_cols
@@ -20,8 +18,9 @@ class TwoTerminalElement(Element):
     def max_node(self):
         return max(self.i, self.j)
 
+
 class FourTerminalElement(Element):
-    def __init__(self,j,jp,k,kp,extra_row_cols=0):
+    def __init__(self, j, jp, k, kp, extra_row_cols=0):
         self.j = j
         self.jp = jp
         self.k = k
@@ -32,72 +31,73 @@ class FourTerminalElement(Element):
     def max_node(self):
         return max(self.j, self.jp, self.k, self.kp)
 
+
 class ResistanceElement(TwoTerminalElement):
-    def __init__(self,i,j,r):
+    def __init__(self, i, j, r):
         self.r = r
-        super().__init__(i,j,1)
+        super().__init__(i, j, 1)
 
     def update(self, mn):
         self.cursor = mn.cursor
-        mn.assign_G(self.i,None, 1)
-        mn.assign_G(None,self.i, 1)
-        mn.assign_G(self.j,None,-1)
-        mn.assign_G(None,self.j,-1)
-        mn.assign_G(None,None, -self.r)
-        mn.incr_cursor()   
+        mn.assign_G(self.i, None, 1)
+        mn.assign_G(None, self.i, 1)
+        mn.assign_G(self.j, None, -1)
+        mn.assign_G(None, self.j, -1)
+        mn.assign_G(None, None, -self.r)
+        mn.incr_cursor()
 
     def sens(self, mn):
         return -mn.Xa[self.cursor]*mn.X[self.cursor]
-        
+
 
 class InductanceElement(TwoTerminalElement):
-    def __init__(self,i,j,l):
+    def __init__(self, i, j, l):
         self.l = l
-        super().__init__(i,j,1)
+        super().__init__(i, j, 1)
 
     def update(self, mn):
         self.cursor = mn.cursor
-        mn.assign_G(self.i,None, 1)
-        mn.assign_G(None,self.i, 1)
-        mn.assign_G(self.j,None,-1)
-        mn.assign_G(None,self.j,-1)
-        mn.assign_C(None,None, -self.l)
-        mn.incr_cursor()   
+        mn.assign_G(self.i, None, 1)
+        mn.assign_G(None, self.i, 1)
+        mn.assign_G(self.j, None, -1)
+        mn.assign_G(None, self.j, -1)
+        mn.assign_C(None, None, -self.l)
+        mn.incr_cursor()
 
     def sens(self, mn):
-        return -mn.Xa[self.cursor]*mn.X[self.cursor]
+        return -mn.s*mn.Xa[self.cursor]*mn.X[self.cursor]
 
 
 class ImpedanceElement(TwoTerminalElement):
-    def __init__(self,i,j,*,r,l):
+    def __init__(self, i, j, *, r, l):
         self.r = r
         self.l = l
-        super().__init__(i,j,1)
+        super().__init__(i, j, 1)
 
     def update(self, mn):
         self.cursor = mn.cursor
-        mn.assign_G(self.i,None, 1)
-        mn.assign_G(None,self.i, 1)
-        mn.assign_G(self.j,None,-1)
-        mn.assign_G(None,self.j,-1)
-        mn.assign_G(None,None, -self.r)
-        mn.assign_C(None,None, -self.l)
-        mn.incr_cursor()   
+        mn.assign_G(self.i, None, 1)
+        mn.assign_G(None, self.i, 1)
+        mn.assign_G(self.j, None, -1)
+        mn.assign_G(None, self.j, -1)
+        mn.assign_G(None, None, -self.r)
+        mn.assign_C(None, None, -self.l)
+        mn.incr_cursor()
 
     def sens(self, mn):
         return -mn.Xa[self.cursor]*mn.X[self.cursor]
 
 
 class ConductanceElement(TwoTerminalElement):
-    def __init__(self,i,j,g):
+    def __init__(self, i, j, g):
         self.g = g
-        super().__init__(i,j)
+        super().__init__(i, j)
 
     def update(self, mn):
-        mn.update_G(self.i,self.i, self.g)
-        mn.update_G(self.j,self.j, self.g)
-        mn.update_G(self.i,self.j,-self.g)
-        mn.update_G(self.j,self.i,-self.g)
+        mn.update_G(self.i, self.i, self.g)
+        mn.update_G(self.j, self.j, self.g)
+        mn.update_G(self.i, self.j, -self.g)
+        mn.update_G(self.j, self.i, -self.g)
 
     def sens(self, mn):
         result = 0
@@ -112,15 +112,15 @@ class ConductanceElement(TwoTerminalElement):
 
 
 class CapacitanceElement(TwoTerminalElement):
-    def __init__(self,i,j,c):
+    def __init__(self, i, j, c):
         self.c = c
-        super().__init__(i,j)
+        super().__init__(i, j)
 
     def update(self, mn):
-        mn.update_C(self.i,self.i, self.c)
-        mn.update_C(self.j,self.j, self.c)
-        mn.update_C(self.i,self.j,-self.c)
-        mn.update_C(self.j,self.i,-self.c)
+        mn.update_C(self.i, self.i, self.c)
+        mn.update_C(self.j, self.j, self.c)
+        mn.update_C(self.i, self.j, -self.c)
+        mn.update_C(self.j, self.i, -self.c)
 
     def sens(self, mn):
         result = 0
@@ -131,129 +131,169 @@ class CapacitanceElement(TwoTerminalElement):
         if self.i > 0 and self.j > 0:
             result -= mn.Xa[self.i-1]*mn.X[self.j-1]
             result -= mn.Xa[self.j-1]*mn.X[self.i-1]
-        return result
+        return mn.s*result
+
 
 class CurrentSourceElement(TwoTerminalElement):
-    def __init__(self,i,j,J):
+    def __init__(self, i, j, J):
         self.J = J
-        super().__init__(i,j)
+        super().__init__(i, j)
 
     def update(self, mn):
         mn.update_W(self.i, -self.J)
         mn.update_W(self.j,  self.J)
 
+    def sens(self, mn):
+        result = 0
+        if self.i > 0:
+            result -= mn.Xa[self.i-1]*mn.W[self.i-1]
+        if self.j > 0:
+            result -= mn.Xa[self.j-1]*mn.W[self.j-1]
+        return result
+
 
 class VoltageSourceElement(TwoTerminalElement):
-    def __init__(self,i,j,E):
+    def __init__(self, i, j, E):
         self.E = E
-        super().__init__(i,j,1)
+        super().__init__(i, j, 1)
 
     def update(self, mn):
         self.cursor = mn.cursor
-        mn.assign_G(self.i,None, 1)
-        mn.assign_G(None,self.i, 1)
-        mn.assign_G(self.j,None,-1)
-        mn.assign_G(None,self.j,-1)
+        mn.assign_G(self.i, None, 1)
+        mn.assign_G(None, self.i, 1)
+        mn.assign_G(self.j, None, -1)
+        mn.assign_G(None, self.j, -1)
         mn.assign_W(None, self.E)
-        mn.incr_cursor()   
+        mn.incr_cursor()
+
+    def sens(self, mn):
+        return -mn.Xa[self.cursor]*mn.W[self.cursor]
+
 
 class VVTElement(FourTerminalElement):
-    def __init__(self,j,jp,k,kp,mu):
+    def __init__(self, j, jp, k, kp, mu):
         self.mu = mu
-        super().__init__(j,jp,k,kp,1)
+        super().__init__(j, jp, k, kp, 1)
 
     def update(self, mn):
         self.cursor = mn.cursor
-        mn.assign_G(None,self.j,-self.mu)
-        mn.assign_G(None,self.jp,self.mu)
-        mn.assign_G(self.k,None,1)
-        mn.assign_G(None,self.k,1)
-        mn.assign_G(self.kp,None,-1)
-        mn.assign_G(None,self.kp,-1)
-        mn.incr_cursor()   
+        mn.assign_G(None, self.j, -self.mu)
+        mn.assign_G(None, self.jp, self.mu)
+        mn.assign_G(self.k, None, 1)
+        mn.assign_G(None, self.k, 1)
+        mn.assign_G(self.kp, None, -1)
+        mn.assign_G(None, self.kp, -1)
+        mn.incr_cursor()
+
+    def sens(self, mn):
+        result = 0
+        if self.j > 0:
+            result -= mn.Xa[self.cursor]*mn.X[self.j-1]
+        if self.jp > 0:
+            result += mn.Xa[self.cursor]*mn.X[self.jp-1]
+        return result
+
 
 class VCTElement(FourTerminalElement):
-    def __init__(self,j,jp,k,kp,g):
+    def __init__(self, j, jp, k, kp, g):
         self.g = g
-        super().__init__(j,jp,k,kp)
+        super().__init__(j, jp, k, kp)
 
     def update(self, mn):
-        mn.update_G(self.k,self.j,self.g)
-        mn.update_G(self.k,self.jp,-self.g)
-        mn.update_G(self.kp,self.j,-self.g)
-        mn.update_G(self.kp,self.jp,self.g)
+        mn.update_G(self.k, self.j, self.g)
+        mn.update_G(self.k, self.jp, -self.g)
+        mn.update_G(self.kp, self.j, -self.g)
+        mn.update_G(self.kp, self.jp, self.g)
+
+    def sens(self, mn):
+        result = 0
+        if self.k > 0 and self.j > 0:
+            result += mn.Xa[self.k-1]*mn.X[self.j-1]
+        if self.k > 0 and self.jp > 0:
+            result -= mn.Xa[self.k-1]*mn.X[self.jp-1]
+        if self.kp > 0 and self.j > 0:
+            result -= mn.Xa[self.kp-1]*mn.X[self.j-1]
+        if self.kp > 0 and self.jp > 0:
+            result += mn.Xa[self.kp-1]*mn.X[self.jp-1]
+
+        return result
+
 
 class OpAmpElement(FourTerminalElement):
-    def __init__(self,j,jp,k,kp):
-        super().__init__(j,jp,k,kp,1)
+    def __init__(self, j, jp, k, kp):
+        super().__init__(j, jp, k, kp, 1)
 
     def update(self, mn):
         self.cursor = mn.cursor
-        mn.assign_G(None,self.j,1)
-        mn.assign_G(None,self.jp,-1)
-        mn.assign_G(self.k,None,1)
-        mn.assign_G(self.kp,None,-1)
-        mn.incr_cursor()   
+        mn.assign_G(None, self.j, 1)
+        mn.assign_G(None, self.jp, -1)
+        mn.assign_G(self.k, None, 1)
+        mn.assign_G(self.kp, None, -1)
+        mn.incr_cursor()
+
+    def sens(self, mn):
+        return None  # No parameters (we could use inverse gain (B))
+
 
 class ModifiedNodal:
-    def __init__( self):
+    def __init__(self):
         self.elements = []
 
     def add(self, el):
-        self.elements.append( el)
+        self.elements.append(el)
 
     def incr_cursor(self):
         self.cursor += 1
         assert self.cursor <= self.n
 
-    def update_G(self,i,j,val):
+    def update_G(self, i, j, val):
         if i is None:
             i = self.cursor+1
         if j is None:
             j = self.cursor+1
-        if i>0 and j>0:
-            self.g_dok[i-1,j-1] = self.g_dok[i-1,j-1] + val
+        if i > 0 and j > 0:
+            self.g_dok[i-1, j-1] = self.g_dok[i-1, j-1] + val
 
-    def assign_G(self,i,j,val):
+    def assign_G(self, i, j, val):
         if i is None:
             i = self.cursor+1
         if j is None:
             j = self.cursor+1
-        if i>0 and j>0:
-            self.g_dok[i-1,j-1] = val
+        if i > 0 and j > 0:
+            self.g_dok[i-1, j-1] = val
 
-    def update_C(self,i,j,val):
+    def update_C(self, i, j, val):
         if i is None:
             i = self.cursor+1
         if j is None:
             j = self.cursor+1
-        if i>0 and j>0:
-            self.c_dok[i-1,j-1] = self.c_dok[i-1,j-1] + val
+        if i > 0 and j > 0:
+            self.c_dok[i-1, j-1] = self.c_dok[i-1, j-1] + val
 
-    def assign_C(self,i,j,val):
+    def assign_C(self, i, j, val):
         if i is None:
             i = self.cursor+1
         if j is None:
             j = self.cursor+1
-        if i>0 and j>0:
-            self.c_dok[i-1,j-1] = val
+        if i > 0 and j > 0:
+            self.c_dok[i-1, j-1] = val
 
-    def update_W(self,i,val):
+    def update_W(self, i, val):
         if i is None:
             i = self.cursor+1
-        if i>0:
+        if i > 0:
             self.W[i-1] += val
 
-    def assign_W(self,i,val):
+    def assign_W(self, i, val):
         if i is None:
             i = self.cursor+1
-        if i>0:
+        if i > 0:
             self.W[i-1] = val
 
-    def semantic( self, output_node):
+    def semantic(self, output_node):
         max_node = 0
         for el in self.elements:
-            max_node = max(max_node,el.max_node())
+            max_node = max(max_node, el.max_node())
 
         self.n = max_node
         for el in self.elements:
@@ -261,15 +301,15 @@ class ModifiedNodal:
 
         self.cursor = max_node
 
-        self.g_dok = dok_matrix( (self.n, self.n))
-        self.c_dok = dok_matrix( (self.n, self.n))
-        self.W = np.zeros( (self.n,))
-        self.d = np.zeros( (self.n,))
+        self.g_dok = dok_matrix((self.n, self.n))
+        self.c_dok = dok_matrix((self.n, self.n))
+        self.W = np.zeros((self.n,))
+        self.d = np.zeros((self.n,))
         if output_node > 0:
             self.d[output_node-1] = 1
 
         for el in self.elements:
-            el.update( self)
+            el.update(self)
 
         assert self.cursor == self.n
 
@@ -279,26 +319,39 @@ class ModifiedNodal:
         del self.g_dok
         del self.c_dok
 
-        print( f"G: {self.G.A}")
-        print( f"C: {self.C.A}")
-        print( f"W: {self.W}")
-        print( f"d: {self.d}")
+        print(f"G: {self.G.A}")
+        print(f"C: {self.C.A}")
+        print(f"W: {self.W}")
+        print(f"d: {self.d}")
 
     def factor(self, *, s=0):
-        self.T = self.G + s*self.C
-        self.lu = sla.splu( self.T)
+        self.s = s
+        self.T = self.G + self.s*self.C
+        self.lu = sla.splu(self.T)
 
     def solve(self):
         self.X = self.lu.solve(self.W)
         return self.X
 
     def solve_adjoint(self):
-        self.Xa = self.lu.solve(-self.d,trans='T')
+        self.Xa = self.lu.solve(-self.d, trans='T')
         return self.Xa
 
     def phi(self):
         return self.d.T.dot(self.X)
 
+    def phase_sens(self, sens):
+        return (sens/self.phi()).imag
+
+    def abs_sens(self, sens):
+        f = self.phi()
+        return np.abs(f)*(sens/f).real
+
+    def abs_sens_napiers(self, sens):
+        return (sens/self.phi()).real
+
+    def abs_sens_dbs(self, sens):
+        return 20*np.log10(np.e)*self.abs_sens_napiers(sens)
+
     def sensitivities(self):
-        for el in self.elements:
-            print( el.sens(self))
+        return [el.sens(self) for el in self.elements]
--- test_modified_nodal.py
+++ test_modified_nodal.py
@@ -2,112 +2,226 @@
 import numpy as np
 from modified_nodal import *
 
+
 def test_Conductance():
     mn = ModifiedNodal()
-    mn.add( ConductanceElement( 1, 0, 2))
-    mn.semantic( 1)
+    mn.add(ConductanceElement(1, 0, 2))
+    mn.semantic(1)
     mn.W[0] = 1
-    mn.factor( s=1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 0.5)
+    assert np.isclose(mn.phi(), 0.5)
     # I=GV; V=I/G; V=IG^-1; V'=-IG^-2=-0.25
     mn.solve_adjoint()
-    assert np.isclose( mn.elements[0].sens(mn), -0.25)
+    assert np.isclose(mn.elements[0].sens(mn), -0.25)
+
 
 def test_Resistance():
     mn = ModifiedNodal()
-    mn.add( ResistanceElement( 1, 0, 2))
-    mn.semantic( 1)
+    mn.add(ResistanceElement(1, 0, 2))
+    mn.semantic(1)
     mn.W[0] = 1
-    mn.factor( s=1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 2.0)
+    assert np.isclose(mn.phi(), 2.0)
     #  V=IR; V'=I=1
     mn.solve_adjoint()
-    assert np.isclose( mn.elements[0].sens(mn), 1)
+    assert np.isclose(mn.elements[0].sens(mn), 1)
+
 
 def test_Capacitance():
     mn = ModifiedNodal()
-    mn.add( CapacitanceElement( 1, 0, 2))
-    mn.semantic( 1)
+    mn.add(CapacitanceElement(1, 0, 2))
+    mn.semantic(1)
     mn.W[0] = 1
-    mn.factor( s=1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 0.5)
+    assert np.isclose(mn.phi(), 0.5)
     # I=CsV; V=I/(Cs); V=(I/s) C^-1; V'=-(I/s) C^-2=-0.25
     mn.solve_adjoint()
-    assert np.isclose( mn.elements[0].sens(mn), -0.25)
+    assert np.isclose(mn.elements[0].sens(mn), -0.25)
+
 
 def test_Inductance():
     mn = ModifiedNodal()
-    mn.add( InductanceElement( 1, 0, 2))
-    mn.semantic( 1)
+    mn.add(InductanceElement(1, 0, 2))
+    mn.semantic(1)
     mn.W[0] = 1
-    mn.factor( s=1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 2.0)
+    assert np.isclose(mn.phi(), 2.0)
     # V=IsL; V'=(Is)=1
     mn.solve_adjoint()
-    assert np.isclose( mn.elements[0].sens(mn), 1)
+    assert np.isclose(mn.elements[0].sens(mn), 1)
+
 
 def test_Impedance():
     mn = ModifiedNodal()
-    mn.add( ImpedanceElement( 1, 0, r=2, l=3))
-    mn.semantic( 1)
+    mn.add(ImpedanceElement(1, 0, r=2, l=3))
+    mn.semantic(1)
     mn.W[0] = 1
-    mn.factor( s=1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 5.0)
+    assert np.isclose(mn.phi(), 5.0)
     # V=IZ; V'=I=1
     mn.solve_adjoint()
-    assert np.isclose( mn.elements[0].sens(mn), 1)
+    assert np.isclose(mn.elements[0].sens(mn), 1)
+
 
 def test_CurrentSource():
     mn = ModifiedNodal()
-    mn.add( ConductanceElement( 1, 0, 2))
-    mn.add( CurrentSourceElement( 0, 1, 1))
-    mn.semantic( 1)
-    mn.factor( s=1)
+    G = 2
+    mn.add(ConductanceElement(1, 0, G))
+    mn.add(CurrentSourceElement(0, 1, 1))
+    mn.semantic(1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 0.5)
+    assert np.isclose(mn.phi(), 0.5)
+    # V=I/G; V'=1/G
+    mn.solve_adjoint()
+    assert np.isclose(mn.elements[1].sens(mn), 1/G)
+
 
 def test_VoltageSource():
     mn = ModifiedNodal()
-    mn.add( VoltageSourceElement( 1, 0, 1))
-    mn.semantic( 1)
-    mn.factor( s=1)
+    mn.add(VoltageSourceElement(1, 0, 1))
+    mn.semantic(1)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), 1.0)
+    assert np.isclose(mn.phi(), 1.0)
+    # V=E; V'=1
+    mn.solve_adjoint()
+    assert np.isclose(mn.elements[0].sens(mn), 1)
+
 
 def test_VVT():
     mn = ModifiedNodal()
-    mn.add( VoltageSourceElement( 1, 0, 1))
-    mn.add( VVTElement( 1, 0, 2, 0, -3))
-    mn.semantic( 2)
-    mn.factor( s=1)
+    E = 1
+    mu = -3
+    mn.add(VoltageSourceElement(1, 0, E))
+    mn.add(VVTElement(1, 0, 2, 0, mu))
+    mn.semantic(2)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), -3.0)
+    assert np.isclose(mn.phi(), E*mu)
+    # V=mu E; V'=E
+    mn.solve_adjoint()
+    assert np.isclose(mn.elements[1].sens(mn), E)
+
 
 def test_VCT():
     mn = ModifiedNodal()
-    mn.add( VoltageSourceElement( 1, 0, 1))
-    mn.add( VCTElement( 1, 0, 0, 2, -3))
-    mn.add( ConductanceElement( 2, 0, 1))
-    mn.semantic( 2)
-    mn.factor( s=1)
+    E = 1
+    g = -3
+    g2 = 2
+    mn.add(VoltageSourceElement(1, 0, E))
+    mn.add(VCTElement(1, 0, 0, 2, g))
+    mn.add(ConductanceElement(2, 0, g2))
+    mn.semantic(2)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), -3.0)
+    # V=E*g/g2
+    assert np.isclose(mn.phi(), E*g/g2)
+    # V'=E/g2
+    mn.solve_adjoint()
+    assert np.isclose(mn.elements[1].sens(mn), E/g2)
+
 
 def test_OpAmp():
     "Inverting Amp"
     mn = ModifiedNodal()
-    mn.add( VoltageSourceElement( 1, 0, 1))
-    mn.add( OpAmpElement( 0, 2, 3, 0))
+    mn.add(VoltageSourceElement(1, 0, 1))
+    mn.add(OpAmpElement(0, 2, 3, 0))
     G1 = 10
     G2 = 2.5
-    mn.add( ConductanceElement( 1, 2, G1))
-    mn.add( ConductanceElement( 2, 3, G2))
-    mn.semantic( 3)
-    mn.factor( s=1)
+    mn.add(ConductanceElement(1, 2, G1))
+    mn.add(ConductanceElement(2, 3, G2))
+    mn.semantic(3)
+    mn.factor(s=1)
     mn.solve()
-    assert np.isclose( mn.phi(), -G1/G2)
+    assert np.isclose(mn.phi(), -G1/G2)
+
+
+def test_ex_6_1_1():
+    "page 174"
+    mn = ModifiedNodal()
+    k = 1
+    G1 = 1
+    G2 = 1
+    C1 = 1
+    C2 = 1
+
+    mn.add(VoltageSourceElement(3, 0, 1))
+    mn.add(ConductanceElement(3, 1, G1))
+    mn.add(ConductanceElement(1, 2, G2))
+    mn.add(CapacitanceElement(2, 0, C2))
+    mn.add(CapacitanceElement(4, 1, C1))
+    mn.add(VVTElement(2, 0, 4, 0, k))
+
+    mn.semantic(2)
+    mn.factor(s=2)
+    mn.solve()
+    assert np.isclose(mn.phi(), 1/9)
+    mn.solve_adjoint()
+    assert np.isclose(mn.elements[1].sens(mn), 2/27)
+
+
+def test_ex_6_4_1():
+    "page 180"
+    mn = ModifiedNodal()
+    J = 1
+    G1 = 1
+    C1 = 0
+    G2 = 0
+    C2 = 1
+    G3 = 1
+    C3 = 0
+
+    mn.add(CurrentSourceElement(0, 1, J))
+    mn.add(ConductanceElement(1, 0, G1))
+    mn.add(CapacitanceElement(1, 0, C1))
+    mn.add(ConductanceElement(1, 2, G2))
+    mn.add(CapacitanceElement(1, 2, C2))
+    mn.add(ConductanceElement(2, 0, G3))
+    mn.add(CapacitanceElement(2, 0, C3))
+
+    mn.semantic(2)
+    mn.factor(s=1j)
+    mn.solve()
+    assert np.isclose(mn.phi(), 0.4+0.2j)
+    mn.solve_adjoint()
+    print(mn.X, mn.Xa)
+    assert np.isclose(mn.elements[2].sens(mn), 1/25-7j/25)
+    assert np.isclose(mn.elements[3].sens(mn), -3/25-4j/25)
+    assert np.isclose(mn.elements[6].sens(mn), 1/25-7j/25)
+
+
+def test_ex_6_5_1():
+    "page 184"
+    mn = ModifiedNodal()
+    J = 1
+    G1 = 4
+    C = 1
+    G2 = 1
+    L = 1
+    g = 3
+
+    mn.add(CurrentSourceElement(0, 1, J))
+    mn.add(ConductanceElement(1, 0, G1))
+    mn.add(CapacitanceElement(1, 0, C))
+    mn.add(ConductanceElement(1, 2, G2))
+    mn.add(InductanceElement(2, 0, L))
+    mn.add(VCTElement(1, 0, 0, 2, g))
+
+    mn.semantic(2)
+    mn.factor(s=1j)
+    mn.solve()
+
+    mn.solve_adjoint()
+    #print(mn.G, mn.C, mn.W, mn.d, mn.X, mn.Xa)
+    assert np.isclose(mn.phi(), (8+16j)/20)
+    d_V2_by_d_L = mn.elements[4].sens(mn)
+    assert np.isclose(d_V2_by_d_L, (272+304j)/400)
+    assert np.isclose(mn.abs_sens(d_V2_by_d_L), 22*np.sqrt(320)/400)
+    assert np.isclose(mn.abs_sens_dbs(d_V2_by_d_L), 20*np.log10(np.e)*22/20)
+    assert np.isclose(mn.phase_sens(d_V2_by_d_L), -6/20)
